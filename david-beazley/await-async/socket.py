61980115d7f669c76058d67ab97dd2e5f2965f76
phunc20 <wucf20@gmail.com>
2020-11-18 22:18:38 +0700

    added, unfinished note

---
+++ david-beazley/await-async/socket.py
@@ -0,0 +1,22 @@
+import socket
+
+def echo_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+    sock.bind(address)
+    sock.listen(1)
+    while True:
+        client, addr = sock.accept()
+        echo_handler(client, addr)
+
+def echo_handler(client, addr):
+    print("Connection from", addr)
+    while client:
+        while True:
+            data = client.recv(100_000)
+            if not data:
+                break
+            client.sendall(data)
+    print("Connection closed")
+
+echo_server(('', 25000))
