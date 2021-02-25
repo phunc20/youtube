d8f17607a12b0a2b395bbb16e65fbb7465725caf
phunc20 <wucf20@gmail.com>
2021-02-25 13:09:20 +0700

    @bind fixed

--- veritasium/logistic_map/notebook.jl
+++ veritasium/logistic_map/notebook.jl
@@ -17,11 +17,13 @@ end
 using Plots
 
 # ╔═╡ f81ced94-76c8-11eb-0e4a-918a418b267d
-#using InteractiveUtils, PlutoUI
-using PlutoUI
+begin
+  using InteractiveUtils
+  using PlutoUI
+end
 
 # ╔═╡ e80655b8-76c5-11eb-1438-85fd5054780b
-next_logistic(x, r=2.6) = r .* x * (1-x) 
+next_logistic(x, growth_rate=2.6) = growth_rate .* x * (1-x) 
 
 # ╔═╡ 54a5bd48-76c6-11eb-18fa-65a57e2be14f
 begin
@@ -32,29 +34,27 @@ end
 # ╔═╡ cf72f62e-76c6-11eb-3389-d343b8217f7b
 plot(x, y, dpi=200)
 
-# ╔═╡ 67b1c086-76c8-11eb-0051-c3dfee2a0722
-macro bind(def, element)
-  quote
-    local el = $(esc(element))
-    global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
-    el
-  end
-end
-
+# ╔═╡ e338d842-7719-11eb-35c6-efdf74875751
+#using BenchmarkTools
 
 # ╔═╡ 41ab4ace-76c8-11eb-1779-eb7d95ed3842
 #@bind n Slider(1:100, default=1, show_value=true)
-@bind n Slider(1:100, show_value=true)
 
-# ╔═╡ e8e090d8-76c8-11eb-02f4-cfc1b36f2637
-n
+# ╔═╡ d3ae7692-7724-11eb-30d6-091451792d46
+md"""
+`n` $(@bind n Slider(1:1000, default=1, show_value=true))
+$(html"<br>")
+`x0` $(@bind x0 Slider(0:0.01:1, default=.4, show_value=true))
+$(html"<br>")
+`r` $(@bind r Slider(0:0.1:999_999_999, default=2.6, show_value=true))
+"""
 
 # ╔═╡ bfd9a498-76c7-11eb-07c7-ad7d1144d4d7
 begin
-  x0 = .4
   xn = x0
   for i in 1:n
-    xn = next_logistic(xn)
+    xn = next_logistic(xn, r)
+	#xn = next_logistic(xn)
   end
   xn
 end
@@ -65,7 +65,7 @@ end
 # ╠═54a5bd48-76c6-11eb-18fa-65a57e2be14f
 # ╠═cf72f62e-76c6-11eb-3389-d343b8217f7b
 # ╠═f81ced94-76c8-11eb-0e4a-918a418b267d
+# ╠═e338d842-7719-11eb-35c6-efdf74875751
 # ╠═41ab4ace-76c8-11eb-1779-eb7d95ed3842
-# ╠═e8e090d8-76c8-11eb-02f4-cfc1b36f2637
+# ╠═d3ae7692-7724-11eb-30d6-091451792d46
 # ╠═bfd9a498-76c7-11eb-07c7-ad7d1144d4d7
-# ╟─67b1c086-76c8-11eb-0051-c3dfee2a0722
